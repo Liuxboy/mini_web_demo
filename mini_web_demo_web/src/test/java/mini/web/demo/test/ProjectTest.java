8cf013786b6714e609438c3ce8005957a155537f
LiuChundong <liuchundong2008@gmail.com>
2015-08-12 17:11:59 +0800

    liuchundong2008@gmail.com 1、add Spring Test Moudel

---
+++ mini_web_demo_web/src/test/java/mini/web/demo/test/ProjectTest.java
@@ -0,0 +1,31 @@
+package mini.web.demo.test;
+
+import liu.chun.dong.service.CalcService;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Resource;
+
+/**
+ * Created In www.jdpay.com
+ *
+ * @author wyliuchundong
+ * @version 1.0.0
+ * @date 2015/8/6 14:38
+ * @comment ProjectTest
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "/META-INF/spring/spring-config.xml",
+        "/META-INF/spring/springMVC-servlet.xml"})
+public class ProjectTest {
+    @Resource
+    private CalcService calcService;
+
+    @Test
+    public void calcVehicleTravelTimeAndDelayTest() {
+        //拼接参数，发送测试数据
+    }
+}
